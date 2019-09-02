42aeafe02b31f8862b4b5dc239bb60798bb15b78
Diana <d7@hotmail.es>
2019-09-02 18:26:55 +0200

    add logic to change color the active menu when the user clicks a project

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,24 @@
 let project = document.getElementsByClassName('project');
-let elementSelected = document.getElementsByClassName('list-group-item');
+let elementSelected = document.querySelectorAll('.list-group-item');
 
-//this function shows the project that is active and hides the others
-//if no project is selected, shows the first
+
+// This function shows the project that is active and hides the others
+// If no project is selected, shows the first
+// Only the clicked element will remain white in the projects menu.
 function showActiveProject () {
   project[0].style.display = "block";
-  for(let i in elementSelected) {
-    elementSelected[i].onclick = function (){
-      project[i].style.display = "block";
-      let elementsToHide = [...project].filter(elem => elem !== project[i])
-      elementsToHide.forEach(elem => elem.style.display = "none");
-    }
+  for(let i = 0; i < elementSelected.length; i++) {
+      elementSelected[i].onclick = function (){
+        for(let j = 0; j < elementSelected.length; j++) {
+          elementSelected[j].style.backgroundColor = '#b88d8c8f';
+          elementSelected[j].style.color = 'white';
+        }
+        elementSelected[i].style.backgroundColor = 'white';
+        elementSelected[i].style.color = '#495057';
+        project[i].style.display = "block";
+        let elementsToHide = [...project].filter(elem => elem !== project[i])
+        elementsToHide.forEach(elem => elem.style.display = "none");
+      }
   }
 }
 
